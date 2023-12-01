cb548fe73d9ac9f8f38535e361ac785c87ed2127
AI4yummy <144452475+AI4yummy@users.noreply.github.com>
2023-12-01 08:39:29 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -6,14 +6,12 @@ result = requests.get(url)
 soup = BeautifulSoup(result.text, "html.parser")
 
 # Find all h4 tags with the specified class
-price_elements = soup.find_all(class_='float-end price card-title pull-right')
-miners = soup.find_all(class_='caption')
-tile = soup.find_all(class_='title')
+listing = soup.find_all(class_='caption')
 
 texts = []
 # Extract text from each element
-for miner in miners:
-    text = miner.get_text()
+for listing in listings:
+    text = listing.get_text()
     texts.append(text)
     print(f"Extracted Text: {text}")
 
